3d1c88eafa863573413296c9acf885f256941cda
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-01-23 16:06:20 +0300

    CSS Modules for CourseInput componenet and add Button's media querry in the CSS file

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,39 @@
+// import { styles } from "ansi-colors";
 import React, { useState } from "react";
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
 // New Component, we can write more than one component per file
 //copy all the styles from the css file, remember to get rid of the selectors
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  // we added this when we started using props:
-  color: ${props => props.invalid ? 'red' : 'black'};
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   // we added this when we started using props:
+//   color: ${props => props.invalid ? 'red' : 'black'};
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-  // we added this when we started using props:
-  background-color: ${props =>  (props.invalid ? '#ffd7d7' : 'transparent')};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//   // we added this when we started using props:
+//   background-color: ${props =>  (props.invalid ? '#ffd7d7' : 'transparent')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
 // Now we can remove these styles now that we're using PROPS!
 // &.invalid input {
@@ -43,7 +44,7 @@ const FormControl = styled.div`
 // &.invalid label {
 //   color: red;
 // }
-`;
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -75,7 +76,7 @@ const CourseInput = (props) => {
   //pay attention to the className attribute!
   // you can repeat the same technique to make more dynamically added classes
   return (
-    <form onSubmit={formSubmitHandler}>
+    <form onSubmit={formSubmitHandler}> 
 
       {/* //////////////////// */}
       {/* This is method ONE for handling styles dynamically: */}
@@ -83,13 +84,14 @@ const CourseInput = (props) => {
       {/* <FormControl className={!isValid && 'invalid'}> */}
 
       {/* Method TWO: using props! */}
-      <FormControl invalid={!isValid}>
+      {/* <FormControl invalid={!isValid}> LAST STEP: commenting this out and going back to div but with CSS MODULES */} 
 
-      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
+      {/* PAY CLOSE ATTENTION TO THE CLASSNAME ATTRIBUTE AND HOW WE HAVE TO USE STYLE AS AN OBJ */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      {/* </div> */}
-      </FormControl>
+      </div>
+      {/* </FormControl> */}
 
 
       <Button type="submit">Add Goal</Button>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,48 +1,49 @@
-// import React from 'react'; //We dont need it anymore bc we're not returing JSX here
-import styled from "styled-components";
+import React from 'react';
+// import styled from "styled-components";
 
-// import './Button.css'; // No need anymore
+import styles from './Button.module.css'; // To use CSS MODULES! Pay attention to how we are importing, and also the addition of "module" the name styles is up to you, you use classes instead
 
 // This Button component was only created to make styling for button components
 // This is called a Tagged Template Literal; This is a DEFAULT JS feature not specific to the styled package or react
 // .button (also other html elements) is method the the styled obj
 // here there's not need for classes so we tweaked the css code we coppied, remove selectors, and for psuedo-selectors add &
 // NOTES ON PROPS! the button returned will by default applies all the props we are passing!
-const Button = styled.button`
-  // default btn width
-  width: 100%;
+// const Button = styled.button`
+//   // default btn width
+//   width: 100%;
 
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  // for devices larger than mobile
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   // for devices larger than mobile
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    // pay attention to className, styles is an object and has all the classes defines in the css file as PROPERTIES!
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
